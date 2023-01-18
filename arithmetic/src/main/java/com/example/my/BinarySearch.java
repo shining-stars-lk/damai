4729daf95476710c93375907461d319fcfaaa30c
阿星不是程序员 <1031900093@qq.com>
2023-01-18 13:58:58 +0800

    添加算法

---
+++ arithmetic/src/main/java/com/example/my/BinarySearch.java
@@ -0,0 +1,30 @@
+package com.example.my;
+
+/**
+ * @program: toolkit
+ * @description:
+ * @author: lk
+ * @create: 2023-01-18
+ **/
+public class BinarySearch {
+    
+    /***
+     * 基本得二分搜索
+     */
+    public int binarySearch(int[] nums, int target){
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }else if (nums[mid] < target) {
+                left = mid + 1;
+            }else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
+ 
