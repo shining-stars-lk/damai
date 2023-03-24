36fd9a40d98ccbf54444a5dc54a3b3bbd4fc2a23
阿星不是程序员 <1031900093@qq.com>
2023-03-24 16:49:14 +0800

    日志审计优化

--- record/src/main/java/com/example/record/aspect/RecordAspect.java
+++ record/src/main/java/com/example/record/aspect/RecordAspect.java
@@ -5,6 +5,7 @@ import com.example.BusinessThreadPool;
 import com.example.core.StringUtil;
 import com.example.record.annotion.Record;
 import com.example.record.es.RecordEsUtils;
+import com.example.record.function.ParseFunctionFactory;
 import com.example.record.provider.RecordBaseInfoProvider;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -21,11 +22,11 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.InetAddress;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * @program: record
  * @description:
  * @author: lk
  * @create: 2023-02-20
@@ -36,9 +37,16 @@ public class RecordAspect extends RecordBaseInfoProvider {
     
     private final Logger logger = LoggerFactory.getLogger(RecordAspect.class);
     
+    public static final String FAIL_CONTENT = "fail_content";
+    
+    public static final String PROCEED = "proceed";
+    
+    public static final String ERROR_MESSAGE = "error_message";
+    
     private RecordEsUtils recordEsUtils;
     
-    public RecordAspect(RecordEsUtils recordEsUtils){
+    public RecordAspect(ParseFunctionFactory parseFunctionFactory, RecordEsUtils recordEsUtils){
+        super(parseFunctionFactory);
         this.recordEsUtils = recordEsUtils;
     }
     
@@ -47,29 +55,57 @@ public class RecordAspect extends RecordBaseInfoProvider {
         RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
         String operatorFlag = request.getHeader(OPERATOR_FLAG);
+        logger.info("record around operatorFlag:{}",operatorFlag);
         if (StringUtil.isNotEmpty(operatorFlag) && OPERATOR_FLAG_YES.equals(operatorFlag))  {
-            Map<String,Object> parameterEsMap = new HashMap<>();
+            Map<String,Object> parameterEsMap = new HashMap<>(64);
             try {
                 addParameter(request,parameterEsMap,joinPoint,record);
-            }catch (Exception e) {
+            }catch (Throwable e) {
                 logger.error("record日志记录生成错误!",e);
             }
-            Object proceed = joinPoint.proceed();
-            try {
-                BusinessThreadPool.execute(() -> {
-                    try {
-                        //返回结果
-                        String result = JSON.toJSONString(proceed);
-                        parameterEsMap.put("result",result);
-                        recordEsUtils.add(parameterEsMap);
-                    }catch (Exception e) {
-                        logger.error("record日志输出到es错误",e);
-                    }
-                });
-            }catch (Exception e) {
-                logger.error("线程池执行record日志输出任务失败",e);
+            long startTimeMillis = System.currentTimeMillis();
+            Map<String,Object> executeMap = new HashMap<>(2);
+            Object proceed = null;
+            String errorMessage = null;
+            try{
+                proceed = joinPoint.proceed();
+                return proceed;
+            }catch (Throwable e) {
+                throw new Exception(e);
+            }finally {
+                executeMap.put(PROCEED,proceed);
+                executeMap.put(ERROR_MESSAGE,errorMessage);
+                parameterEsMap.put("execution_time",System.currentTimeMillis() - startTimeMillis);
+                parameterEsMap.put("end_time", System.currentTimeMillis());
+                try {
+                    BusinessThreadPool.execute(() -> {
+                        try {
+                            //返回结果
+                            if (executeMap.get(PROCEED) != null) {
+                                String result = JSON.toJSONString(executeMap.get(PROCEED));
+                                parameterEsMap.put("result",result);
+                            }
+                            //异常错误信息
+                            if (executeMap.get(ERROR_MESSAGE) != null) {
+                                parameterEsMap.put(ERROR_MESSAGE,executeMap.get(ERROR_MESSAGE));
+                            }
+                            //执行content中executeBefore=false的apply函数
+                            Object content = parameterEsMap.get("content");
+                            if (content != null) {
+                                String contentNewest = executeFunction((String) content, joinPoint, false);
+                                parameterEsMap.put("content",contentNewest);
+                            }
+                            //处理failContent
+                            dealFailContent(joinPoint, executeMap, parameterEsMap);
+                            recordEsUtils.add(parameterEsMap);
+                        }catch (Throwable e) {
+                            logger.error("record日志输出到es错误",e);
+                        }
+                    });
+                }catch (Throwable e) {
+                    logger.error("线程池执行record日志输出任务失败",e);
+                }
             }
-            return proceed;
         }else {
             return joinPoint.proceed();
         }
@@ -86,8 +122,12 @@ public class RecordAspect extends RecordBaseInfoProvider {
         String contentType = request.getHeader("Content-Type");
         parameterEsMap.put("content_type",contentType);
         //请求体
-        String requestBody = JSON.toJSONString(requestBodyContent);
-        parameterEsMap.put("request_body",requestBody);
+        if (StringUtil.isNotEmpty(contentType) && contentType.contains("multipart/form-data")) {
+            parameterEsMap.put("request_body","this is a file");
+        }else {
+            String requestBody = JSON.toJSONString(requestBodyContent);
+            parameterEsMap.put("request_body",requestBody);
+        }
         //请求方式
         String method = request.getMethod();
         parameterEsMap.put("method",method);
@@ -98,14 +138,42 @@ public class RecordAspect extends RecordBaseInfoProvider {
         //请求地址
         String requestURI = request.getRequestURI();
         parameterEsMap.put("request_uri",requestURI);
+        //版本号
+        String header = request.getHeader("api-version");
+        parameterEsMap.put( "api_version",header);
         //ip地址
         String ipAddress = InetAddress.getLocalHost().getHostAddress();
         parameterEsMap.put("ip_address",ipAddress);
         //时间戳
-        parameterEsMap.put("@timestamp",new Date());
+        parameterEsMap.put("@timestamp",System.currentTimeMillis());
         //日志内容
         String definitionContent = getDefinitionContent(joinPoint, record.content());
-        String content = executeFunction(definitionContent, joinPoint);
+        //执行content中executeBefore=true的apply函数
+        String content = executeFunction(definitionContent, joinPoint, true);
         parameterEsMap.put("content",content);
+        //执行failContent中executeBefore=true的apply函数
+        if (StringUtil.isNotEmpty(record.failContent())) {
+            parameterEsMap.put(FAIL_CONTENT,executeFunction(record.failContent(), joinPoint, true));
+        }
+        
+    }
+    
+    /**
+     * 处理failContent
+     * */
+    public void dealFailContent(ProceedingJoinPoint joinPoint, Map<String,Object> executeMap, Map<String,Object> parameterEsMap) throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        Object failContent = parameterEsMap.remove(FAIL_CONTENT);
+        if (failContent != null) {
+            //执行failContent中executeBefore=false的apply函数
+            String failContentNewest = executeFunction((String) failContent, joinPoint, false);
+            //异常信息进行填充
+            if (parameterEsMap.get(FAIL_CONTENT) == null && parameterEsMap.get(ERROR_MESSAGE) != null) {
+                failContentNewest = getDefinitionContent(joinPoint,failContentNewest,(String)parameterEsMap.get(ERROR_MESSAGE));
+                parameterEsMap.put(FAIL_CONTENT,failContentNewest);
+            }
+            if (parameterEsMap.get(FAIL_CONTENT) != null) {
+                parameterEsMap.remove("content");
+            }
+        }
     }
 }
