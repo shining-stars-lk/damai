d332c4075bbcf0e6028dfc38229b4caf9ff440ce
shining-stars-lk <1031900093@qq.com>
2023-04-19 19:59:55 +0800

    添加模块

---
+++ course-case/src/main/java/com/example/CourseCaseApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseCaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseCaseApplication.class, args);
+    }
+
+}
