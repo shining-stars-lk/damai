906ecc543e3fe67db4b5edaaad710423b8fcfe08
shining-stars-lk <1031900093@qq.com>
2023-04-23 19:35:09 +0800

    添加课程案例

--- course-case/src/main/java/com/example/synchronizedcase/SynchronizedCase6.java
+++ course-case/src/main/java/com/example/synchronizedcase/SynchronizedCase6.java
@@ -4,26 +4,32 @@ package com.example.synchronizedcase;
  * @program: toolkit
  * @description: 
  * 开启2个线程，一个线程中加上synchronized对象锁，循环50000次对result变量进行++自增，
- * 另一个线程不加synchronized，循环50000次对result变量进行++自增，
+ * 另一个线程加synchronized到此class上，循环50000次对result变量进行++自增，
  * 主线程等待10s后输出result值结果 
  * @author: lk
  * @create: 2023-04-23
  **/
-public class SynchronizedCase5 {
+public class SynchronizedCase6 {
     private static Integer result = 0;
     
     public static void main(String[] args) {
-        Object object = new Object();
+        SynchronizedCase6 synchronizedCase6 = new SynchronizedCase6();
+        synchronizedCase6.test();
+    }
+    
+    public void test(){
         new Thread(() -> {
-            synchronized (object) {
+            synchronized (this) {
                 for (int j = 0; j < 50000; j++) {
                     result++;
                 }
             }
         }).start();
         new Thread(() -> {
-            for (int j = 0; j < 50000; j++) {
-                result++;
+            synchronized (SynchronizedCase6.class) {
+                for (int j = 0; j < 50000; j++) {
+                    result++;
+                }
             }
         }).start();
         try {
