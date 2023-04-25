85f2ec763d73f50007a676aa03e878b8526b3cc9
shining-stars-lk <1031900093@qq.com>
2023-04-25 18:00:50 +0800

    id生成器修改

--- course-case/src/main/java/com/example/servecase/entity/Test.java
+++ course-case/src/main/java/com/example/servecase/entity/Test.java
@@ -1,12 +1,15 @@
 package com.example.servecase.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class Test {
-
+    
+    @TableId(type = IdType.ASSIGN_ID)
     private Long id;
 
     private String column1;
