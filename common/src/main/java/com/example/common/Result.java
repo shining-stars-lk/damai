46ba6a94c5f2f4ae8c61d2e0c5bed3b0fc165dfc
shining-stars-lk <1031900093@qq.com>
2023-05-04 14:07:51 +0800

    添加加密

--- common/src/main/java/com/example/common/Result.java
+++ common/src/main/java/com/example/common/Result.java
@@ -44,7 +44,7 @@ public class Result<T> implements Serializable {
     
     public static <T> Result<T> success(T t) {
         Result<T> result = new Result<T>();
-        result.code = 1;
+        result.code = 0;
         result.setData(t);
         return result;
     }
