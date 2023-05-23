b09c6a2da7eba2e05cb152947cf6564ba080bad3
shining-stars-lk <1031900093@qq.com>
2023-05-23 15:55:29 +0800

    优化id生成器

---
+++ id-generator/src/main/java/com/baidu/fsg/uid/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.baidu.fsg.uid.config;
+
+import com.baidu.fsg.uid.worker.WorkerIdAssigner;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @program: toolkit
+ * @description:
+ * @author: lk
+ * @create: 2023-05-23
+ **/
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnProperty("spring.redis.host")
+public class RedisConfig {
+    
+    @Bean("myRedisTemplate")
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+    
+    @Bean("disposableWorkerIdAssigner")
+    public WorkerIdAssigner redisDisposableWorkerIdAssigner(@Qualifier("myRedisTemplate") RedisTemplate redisTemplate){
+        RedisDisposableWorkerIdAssigner redisDisposableWorkerIdAssigner = new RedisDisposableWorkerIdAssigner(redisTemplate);
+        return redisDisposableWorkerIdAssigner;
+    }
+}
---
+++ id-generator/src/main/java/com/baidu/fsg/uid/config/RedisDisposableWorkerIdAssigner.java
@@ -0,0 +1,30 @@
+package com.baidu.fsg.uid.config;
+
+import com.baidu.fsg.uid.worker.WorkerIdAssigner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @program: toolkit
+ * @description:
+ * @author: lk
+ * @create: 2023-05-23
+ **/
+public class RedisDisposableWorkerIdAssigner implements WorkerIdAssigner {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(RedisDisposableWorkerIdAssigner.class);
+    
+    private RedisTemplate redisTemplate;
+    
+    public RedisDisposableWorkerIdAssigner (RedisTemplate redisTemplate){
+        this.redisTemplate = redisTemplate;
+    }
+    
+    @Override
+    public long assignWorkerId() {
+        String key = "uid_work_id";
+        Long increment = redisTemplate.opsForValue().increment(key);
+        return increment;
+    }
+}
