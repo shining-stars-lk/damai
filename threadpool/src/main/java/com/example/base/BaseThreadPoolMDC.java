2682ee1233b6fd56d5edcef58a20a254e80106b0
阿星不是程序员 <1031900093@qq.com>
2023-06-06 11:20:54 +0800

    修复多线程下traceId传递

--- threadpool/src/main/java/com/example/base/BaseThreadPoolMDC.java
+++ threadpool/src/main/java/com/example/base/BaseThreadPoolMDC.java
@@ -3,6 +3,7 @@ package com.example.base;
 import com.example.threadlocal.BaseParameterHolder;
 import org.slf4j.MDC;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
@@ -32,46 +33,19 @@ public class BaseThreadPoolMDC {
      * 对要执行的execute任务进行包装
      *
      * @param runnable 任务
-     * @param context  父线程的MDC上下文
+     * @param parentMdcContext 父线程的MDC上下文
+     * @param parentHoldContext 父线程的hold上下文
      */
-    protected static Runnable wrapExecute(final Runnable runnable, final Map<String, String> context, final Map<String, String> holdContext) {
+    protected static Runnable wrapExecute(final Runnable runnable, final Map<String, String> parentMdcContext, final Map<String, String> parentHoldContext) {
         return () -> {
-            //获取本线程的hold上下文
-            Map<String, String> parameterMap = BaseParameterHolder.getParameterMap();
-            //获取本线程的MDC上下文
-            Map<String, String> previous = MDC.getCopyOfContextMap();
-            //如果父线程的MDC上下文为空，则清空子线程的
-            if (context == null) {
-                MDC.clear();
-            } else {
-                //否则将父线程的设置到这次本线程中
-                MDC.setContextMap(context);
-            }
-            //如果父线程的hold上下文为空，则清空子线程的
-            if (holdContext == null) {
-                BaseParameterHolder.removeParameterMap();
-            } else {
-                //否则将父线程的设置到这次本线程中
-                BaseParameterHolder.setParameterMap(holdContext);
-            }
+            Map<String, Map<String, String>> preprocess = preprocess(parentMdcContext, parentHoldContext);
+            Map<String, String> holdContext = preprocess.get("holdContext");
+            Map<String, String> mdcContext = preprocess.get("mdcContext");
             try {
                 //执行任务
                 runnable.run();
             } finally {
-                //如果本线程MDC上下文为空，直接清除掉
-                if (previous == null) {
-                    MDC.clear();
-                } else {
-                    //否则，将本线程的上下文恢复回去
-                    MDC.setContextMap(previous);
-                }
-                //如果本线程hold上下文为空，直接清除掉
-                if (parameterMap == null) {
-                    BaseParameterHolder.removeParameterMap();
-                } else {
-                    //否则，将本线程的上下文恢复回去
-                    BaseParameterHolder.setParameterMap(parameterMap);
-                }
+                postProcess(mdcContext,holdContext);
             }
         };
     }
@@ -80,47 +54,62 @@ public class BaseThreadPoolMDC {
      * 对要执行的submit任务进行包装
      *
      * @param task    任务
-     * @param context 父线程的MDC上下文
+     * @param parentMdcContext 父线程的MDC上下文
+     * @param parentHoldContext 父线程的hold上下文
      */
-    protected static <T> Callable<T> wrapSubmit(Callable<T> task, final Map<String, String> context, final Map<String, String> holdContext) {
+    protected static <T> Callable<T> wrapSubmit(Callable<T> task, final Map<String, String> parentMdcContext, final Map<String, String> parentHoldContext) {
         return () -> {
-            //获取本线程的hold上下文
-            Map<String, String> parameterMap = BaseParameterHolder.getParameterMap();
-            //获取本线程的MDC上下文
-            Map<String, String> previous = MDC.getCopyOfContextMap();
-            //如果父线程的MDC上下文为空，则清空子线程的
-            if (context == null) {
-                MDC.clear();
-            } else {
-                //否则将父线程的设置到这次本线程中
-                MDC.setContextMap(context);
-            }
-            //如果父线程的hold上下文为空，则清空子线程的
-            if (holdContext == null) {
-                BaseParameterHolder.removeParameterMap();
-            } else {
-                //否则将父线程的设置到这次本线程中
-                BaseParameterHolder.setParameterMap(holdContext);
-            }
+            Map<String, Map<String, String>> preprocess = preprocess(parentMdcContext, parentHoldContext);
+            Map<String, String> holdContext = preprocess.get("holdContext");
+            Map<String, String> mdcContext = preprocess.get("mdcContext");
             try {
                 //执行任务
                 return task.call();
             } finally {
-                //如果本线程MDC上下文为空，直接清除掉
-                if (previous == null) {
-                    MDC.clear();
-                } else {
-                    //否则，将本线程的上下文恢复回去
-                    MDC.setContextMap(previous);
-                }
-                //如果本线程hold上下文为空，直接清除掉
-                if (parameterMap == null) {
-                    BaseParameterHolder.removeParameterMap();
-                } else {
-                    //否则，将本线程的上下文恢复回去
-                    BaseParameterHolder.setParameterMap(parameterMap);
-                }
+                postProcess(mdcContext,holdContext);
             }
         };
+    }
+    
+    private static Map<String,Map<String,String>> preprocess(final Map<String, String> parentMdcContext, final Map<String, String> parentHoldContext){
+        Map<String,Map<String,String>> map = new HashMap<>();
+        //获取本线程的hold上下文
+        Map<String, String> holdContext = BaseParameterHolder.getParameterMap();
+        //获取本线程的MDC上下文
+        Map<String, String> mdcContext = MDC.getCopyOfContextMap();
+        //如果父线程的MDC上下文为空，则清空子线程的
+        if (parentMdcContext == null) {
+            MDC.clear();
+        } else {
+            //否则将父线程的设置到这次本线程中
+            MDC.setContextMap(parentMdcContext);
+        }
+        //如果父线程的hold上下文为空，则清空子线程的
+        if (parentHoldContext == null) {
+            BaseParameterHolder.removeParameterMap();
+        } else {
+            //否则将父线程的设置到这次本线程中
+            BaseParameterHolder.setParameterMap(parentHoldContext);
+        }
+        map.put("holdContext",holdContext);
+        map.put("mdcContext",mdcContext);
+        return map;
+    }
+    
+    private static void postProcess(Map<String, String> mdcContext, Map<String, String> holdContext){
+        //如果本线程MDC上下文为空，直接清除掉
+        if (mdcContext == null) {
+            MDC.clear();
+        } else {
+            //否则，将本线程的上下文恢复回去
+            MDC.setContextMap(mdcContext);
+        }
+        //如果本线程hold上下文为空，直接清除掉
+        if (holdContext == null) {
+            BaseParameterHolder.removeParameterMap();
+        } else {
+            //否则，将本线程的上下文恢复回去
+            BaseParameterHolder.setParameterMap(holdContext);
+        }
     }
 }
