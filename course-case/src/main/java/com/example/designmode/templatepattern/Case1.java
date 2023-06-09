b8ebc74c74b092d45e2f0af2d07c2ee181485bac
阿星不是程序员 <1031900093@qq.com>
2023-06-09 13:59:56 +0800

    添加案例

---
+++ course-case/src/main/java/com/example/designmode/templatepattern/Case1.java
@@ -0,0 +1,16 @@
+package com.example.designmode.templatepattern;
+
+/**
+ * @program: toolkit
+ * @description:
+ * @author: k
+ * @create: 2023-06-09
+ **/
+public class Case1 {
+    public static void main(String[] args) {
+        Fruit fruit1 = new Apple();
+        fruit1.eat();
+        Fruit fruit2 = new Banana();
+        fruit2.eat();
+    }
+}
---
+++ course-case/src/main/java/com/example/designmode/templatepattern/Fruit.java
@@ -0,0 +1,30 @@
+package com.example.designmode.templatepattern;
+
+/**
+ * @program: toolkit
+ * @description:
+ * @author: k
+ * @create: 2023-06-09
+ **/
+public abstract class Fruit {
+    
+    public abstract String getFruitName();
+    
+    public abstract void processFruit();
+    
+    
+    public void buyFruit(){
+        System.out.println("购买水果 : " + getFruitName());
+    }
+    
+    public void finishEatFruit(){
+        System.out.println("吃到了水果 : " + getFruitName());
+    }
+    
+    
+    public void eat() {
+        buyFruit();
+        processFruit();
+        finishEatFruit();
+    }
+}
