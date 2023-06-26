628f1740fdfcfc9a6806a109d94ac0bb1412f287
阿星不是程序员 <1031900093@qq.com>
2023-06-26 16:12:38 +0800

    添加监控

---
+++ server/admin/src/main/java/com/example/config/MonitorServerConfig.java
@@ -0,0 +1,31 @@
+package com.example.config;
+
+import com.example.monitor.DingTalkMessage;
+import com.example.monitor.MonitorServer;
+import de.codecentric.boot.admin.server.domain.entities.InstanceRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @program: toolkit
+ * @description:
+ * @author: k
+ * @create: 2023-06-26
+ **/
+public class MonitorServerConfig {
+    
+    @Value("${dingtalk.token:}")
+    private String token;
+    
+    @Bean
+    public DingTalkMessage dingTalkMessage(){
+        return new DingTalkMessage(token);
+    }
+    
+    @Bean
+    public MonitorServer monitorServer(DingTalkMessage dingTalkMessage,InstanceRepository repository){
+        return new MonitorServer(dingTalkMessage,repository);
+    }
+    
+    
+}
