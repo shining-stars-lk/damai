72d2682cf6c6da90640eb186741af8791baae4c6
shining-stars-lk <1031900093@qq.com>
2023-06-29 11:13:04 +0800

    添加任务异步

---
+++ common/src/main/java/com/example/enums/JobRunType.java
@@ -0,0 +1,24 @@
+package com.example.enums;
+
+/**
+ * @program: toolkit
+ * @description:
+ * @author: k
+ * @create: 2023-06-03
+ **/
+public enum JobRunType {
+    /**
+     * 同步执行
+     * */
+    SYNC_RUN,
+    
+    /**
+     * 异步执行
+     * */
+    ASYNC_RUN;
+    
+    JobRunType() {
+       
+    }
+    
+}
