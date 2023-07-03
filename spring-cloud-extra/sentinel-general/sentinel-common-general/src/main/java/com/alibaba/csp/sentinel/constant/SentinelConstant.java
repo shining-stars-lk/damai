0587a8b650ae64a14c9f2f9bbb61d2ade34a5735
阿星不是程序员 <1031900093@qq.com>
2023-07-03 22:51:25 +0800

    添加sentinel通过规则

---
+++ spring-cloud-extra/sentinel-general/sentinel-common-general/src/main/java/com/alibaba/csp/sentinel/constant/SentinelConstant.java
@@ -0,0 +1,14 @@
+package com.alibaba.csp.sentinel.constant;
+
+/**
+ * @program: toolkit
+ * @description:
+ * @author: k
+ * @create: 2023-07-03
+ **/
+public class SentinelConstant {
+    
+    public static final String SPLIT_RESOURCE = "_";
+    
+    public static final String GENERAL_URL = "#general";
+}
