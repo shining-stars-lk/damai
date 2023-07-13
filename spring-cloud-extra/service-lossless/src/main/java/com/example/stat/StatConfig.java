1bafb622fe858a077c9525f8fd05ba69bf2550e3
阿星不是程序员 <1091900093@qq.com>
2023-07-13 18:35:52 +0800

    添加服务切换无损耗功能

---
+++ spring-cloud-extra/service-lossless/src/main/java/com/example/stat/StatConfig.java
@@ -0,0 +1,23 @@
+package com.example.stat;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @program: toolkit
+ * @description:
+ * @author: k
+ * @create: 2023-07-13
+ **/
+public class StatConfig {
+    
+    @Bean
+    public StatIndicator statIndicator(MeterRegistry registry){
+        return new StatIndicator(registry);
+    }
+    
+    @Bean
+    public StatFilter statFilter(StatIndicator statIndicator){
+        return new StatFilter(statIndicator);
+    }
+}
---
+++ spring-cloud-extra/service-lossless/src/main/java/com/example/stat/StatIndicator.java
@@ -0,0 +1,29 @@
+package com.example.stat;
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+
+/**
+ * @program: bjgoodwill-msa-scloud
+ * @description:
+ * @author: lk
+ * @create: 2022-11-21
+ **/
+
+public class StatIndicator {
+    
+    private Counter apiRequestCounter;
+    
+    public StatIndicator(MeterRegistry registry) {
+        /**
+         * 创建请求数自定义指标
+         * 计数器可以用于记录只会增加不会减少的指标类型，比如记录应用请求的总量(api_request_total)，
+         * 一般而言，Counter类型的metrics指标在命名中，我们使用_total结束。
+         */
+        apiRequestCounter = registry.counter("api_request_total", "application", "log-service");
+    }
+    
+    public void increment(){
+        apiRequestCounter.increment();
+    }
+}
