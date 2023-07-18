da3e9e8bd4e1101cdea0e815fe6251b1515a5cbe
阿星不是程序员 <1031900093@qq.com>
2023-07-18 16:07:39 +0800

    添加文件

--- id-generator/src/main/java/com/baidu/fsg/uid/config/IdGeneratorRedisConfig.java
+++ id-generator/src/main/java/com/baidu/fsg/uid/config/IdGeneratorRedisConfig.java
@@ -11,7 +11,8 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * @program: cook-frame
- * @description:
+ * @description: 对百度开源id生成器进行redis适配
+ * @see <a href="https://github.com/baidu/uid-generator/">百度开源id生成器</a>
  * @author: 星哥
  * @create: 2023-05-23
  **/
