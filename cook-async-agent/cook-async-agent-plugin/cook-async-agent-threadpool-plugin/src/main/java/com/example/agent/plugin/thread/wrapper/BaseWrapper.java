f1d1895427c113cda57f0e08013cf986bec4bb01
阿星不是程序员 <1031900093@qq.com>
2023-07-21 19:34:41 +0800

    添加字节码增强

---
+++ cook-async-agent/cook-async-agent-plugin/cook-async-agent-threadpool-plugin/src/main/java/com/example/agent/plugin/thread/wrapper/BaseWrapper.java
@@ -0,0 +1,48 @@
+package com.example.agent.plugin.thread.wrapper;
+
+import com.example.ParameterHolder;
+import org.slf4j.MDC;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @program: cook-frame
+ * @description:
+ * @author: k
+ * @create: 2023-07-21
+ **/
+public class BaseWrapper {
+    
+    protected static Map<String, Map<String,String>> preprocess(final Map<String, String> parentMdcContext, final Map<String, String> parentHoldContext){
+        Map<String,Map<String,String>> map = new HashMap<>();
+        Map<String, String> holdContext = ParameterHolder.getParameterMap();
+        Map<String, String> mdcContext = MDC.getCopyOfContextMap();
+        if (parentMdcContext == null) {
+            MDC.clear();
+        } else {
+            MDC.setContextMap(parentMdcContext);
+        }
+        if (parentHoldContext == null) {
+            ParameterHolder.removeParameterMap();
+        } else {
+            ParameterHolder.setParameterMap(parentHoldContext);
+        }
+        map.put("holdContext",holdContext);
+        map.put("mdcContext",mdcContext);
+        return map;
+    }
+    
+    protected static void postProcess(Map<String, String> mdcContext, Map<String, String> holdContext){
+        if (mdcContext == null) {
+            MDC.clear();
+        } else {
+            MDC.setContextMap(mdcContext);
+        }
+        if (holdContext == null) {
+            ParameterHolder.removeParameterMap();
+        } else {
+            ParameterHolder.setParameterMap(holdContext);
+        }
+    }
+}
