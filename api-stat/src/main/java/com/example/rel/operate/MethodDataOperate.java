04c62aeeb48fcec2afa4e1879e90df9106180721
shining-stars-lk <1031900093@qq.com>
2023-09-28 13:38:32 +0800

    优化结构

---
+++ api-stat/src/main/java/com/example/rel/operate/MethodDataOperate.java
@@ -0,0 +1,52 @@
+package com.example.rel.operate;
+
+import com.example.rel.MethodDataStackHolder;
+import com.example.rel.structure.MethodData;
+import com.example.util.ApiStatCommon;
+import com.example.util.MethodType;
+import lombok.AllArgsConstructor;
+import org.aopalliance.intercept.MethodInvocation;
+
+import java.util.Stack;
+
+/**
+ * @program: cook-frame
+ * @description:
+ * @author: k
+ * @create: 2023-09-28
+ **/
+@AllArgsConstructor
+public class MethodDataOperate {
+    
+    private final MethodDataStackHolder methodDataStackHolder;
+    
+    
+    public MethodData getParentMethodData() {
+        Stack<MethodData> stack = methodDataStackHolder.getMethodData();
+        if (stack == null) {
+            MethodData methodData = new MethodData();
+            methodData.setId("org.springframework.web.servlet.DispatcherServlet.doDispatch");
+            methodData.setClassName("DispatcherServlet");
+            methodData.setMethodName("doDispatch");
+            methodData.setMethodType(MethodType.Dispatcher);
+            return methodData;
+        }
+        return stack.peek();
+    }
+    
+    public MethodData getCurrentMethodNode(MethodInvocation pjp, double runTime) {
+        String className = pjp.getMethod().getDeclaringClass().getName();
+        String methodName = pjp.getMethod().getName();
+        MethodData methodData = new MethodData();
+        methodData.setId(className + "." + methodName);
+        methodData.setClassName(className);
+        methodData.setMethodName(methodName);
+        //BigDecimal bg = BigDecimal.valueOf(runTime);
+        //runTime = bg.setScale(2, RoundingMode.HALF_UP).doubleValue()
+        methodData.setMethodType(ApiStatCommon.getMethodType(pjp));
+        if (methodData.getMethodType()==MethodType.Controller) {
+            methodData.setApi(ApiStatCommon.getRoute(pjp));
+        }
+        return methodData;
+    }
+}
