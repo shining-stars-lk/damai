b8522cf13a4619e747b6f4893f01320b5886a986
阿星不是程序员 <1031900093@qq.com>
2023-09-30 22:19:48 +0800

    优化结构

--- api-stat/api-stat-system/src/main/java/com/example/ApiStatSystemApplication.java
+++ api-stat/api-stat-system/src/main/java/com/example/ApiStatSystemApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class AouStatSystemApplication {
+public class ApiStatSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AouStatSystemApplication.class, args);
+        SpringApplication.run(ApiStatSystemApplication.class, args);
     }
 
 }
