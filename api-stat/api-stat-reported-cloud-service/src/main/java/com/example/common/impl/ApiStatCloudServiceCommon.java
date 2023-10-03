209cf7b1ac2536f8acab53887228a483c555ea6e
shining-stars-lk <1031900093@qq.com>
2023-10-03 22:32:47 +0800

    优化结构

---
+++ api-stat/api-stat-reported-cloud-service/src/main/java/com/example/common/impl/ApiStatCloudServiceCommon.java
@@ -0,0 +1,39 @@
+package com.example.common.impl;
+
+import com.example.common.ApiStatCommon;
+import com.example.enums.MethodLevel;
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.example.constant.ApiStatConstant.*;
+import static com.example.constant.ApiStatConstant.MAPPER;
+
+public class ApiStatCloudServiceCommon implements ApiStatCommon {
+    @Override
+    public MethodLevel getMethodLevel(MethodInvocation pjp) {
+        Class<?> targetClass = pjp.getThis().getClass();
+        if (targetClass.getAnnotation(Controller.class) != null || targetClass.getAnnotation(RestController.class) != null) {
+            return MethodLevel.Controller;
+        } else if (targetClass.getAnnotation(Service.class) != null) {
+            return MethodLevel.Service;
+        } else if (targetClass.getAnnotation(Repository.class) != null) {
+            return MethodLevel.Dao;
+        } else if (targetClass.getAnnotation(FeignClient.class) != null) {
+            return MethodLevel.Dao;
+        }
+        String className = pjp.getMethod().getDeclaringClass().getName().toLowerCase();
+        if (className.contains(CONTROLLER)) {
+            return MethodLevel.Controller;
+        } else if (className.contains(SERVICE)) {
+            return MethodLevel.Service;
+        } else if (className.contains(DAO) || className.contains(MAPPER) || className.contains(FEIGN)) {
+            return MethodLevel.Dao;
+        } else {
+            return MethodLevel.Others;
+        }
+    }
+}
