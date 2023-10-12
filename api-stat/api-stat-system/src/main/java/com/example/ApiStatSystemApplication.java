849465ba61612d98d193f359d4996376f2ae6062
shining-stars-lk <1031900093@qq.com>
2023-10-12 16:17:38 +0800

    添加示例

--- api-stat/api-stat-system/src/main/java/com/example/ApiStatSystemApplication.java
+++ api-stat/api-stat-system/src/main/java/com/example/ApiStatSystemApplication.java
@@ -2,8 +2,9 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@EnableScheduling
 @SpringBootApplication
 public class ApiStatSystemApplication {
 
---
+++ api-stat/api-stat-system/src/main/java/com/example/task/NoticeTask.java
@@ -0,0 +1,26 @@
+package com.example.task;
+
+import com.example.notice.ApiStatNotice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import static com.example.constant.ApiStatConstant.PLATFORM_NOTICE;
+
+/**
+ * @program: cook-frame
+ * @description:
+ * @author: k
+ * @create: 2023-10-12
+ **/
+@Component
+public class NoticeTask {
+    
+    @Autowired
+    private ApiStatNotice apiStatNotice;
+    
+    @Scheduled(cron ="*/6 * * * * ?")
+    public void scheduleNotice() {
+        apiStatNotice.notice(PLATFORM_NOTICE);
+    }
+}
