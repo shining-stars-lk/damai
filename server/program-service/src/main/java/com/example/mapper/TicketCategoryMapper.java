8a97c7ca41f45ccc1b515fc2f49c265d30883af0
shining-stars-lk <1031900093@qq.com>
2024-01-08 10:33:17 +0800

    添加节目模块

---
+++ server/program-service/src/main/java/com/example/mapper/TicketCategoryMapper.java
@@ -0,0 +1,16 @@
+package com.example.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.entity.TicketCategory;
+
+/**
+ * <p>
+ * 节目票档表 Mapper 接口
+ * </p>
+ *
+ * @author k
+ * @since 2024-01-08
+ */
+public interface TicketCategoryMapper extends BaseMapper<TicketCategory> {
+
+}
