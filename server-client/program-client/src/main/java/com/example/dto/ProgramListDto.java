6f2bae78f55c96db0339a44fdbb22e29fa4c9e73
阿星不是程序员 <1031900093@qq.com>
2024-01-09 20:05:10 +0800

    首页列表功能

---
+++ server-client/program-client/src/main/java/com/example/dto/ProgramListDto.java
@@ -0,0 +1,32 @@
+package com.example.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @program: cook-frame
+ * @description:
+ * @author: 星哥
+ * @create: 2023-07-05
+ **/
+@Data
+@ApiModel(value="ProgramListDto", description ="主页节目列表")
+public class ProgramListDto {
+    
+    @ApiModelProperty(name ="areaId", dataType ="Long", value ="所在区域id")
+    private Long areaId;
+    
+    @ApiModelProperty(name ="programCategoryIds", dataType ="Long[]", value ="节目类型id集合")
+    @NotNull
+    private List<Long> programCategoryIds;
+    
+    /**
+     * 业务字段，后端自己填充
+     * */
+    private Date time;
+}
