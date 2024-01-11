63c95b3850730266963f7cdc7601d98634a7b675
shining-stars-lk <1031900093@qq.com>
2024-01-11 17:54:49 +0800

    首页列表功能

---
+++ server/program-service/src/main/java/com/example/entity/TicketCategoryAggregate.java
@@ -0,0 +1,33 @@
+package com.example.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * <p>
+ * 节目票档表(聚合查询使用)
+ * </p>
+ *
+ * @author k
+ * @since 2024-01-08
+ */
+@Data
+public class TicketCategoryAggregate implements Serializable {
+    
+    /**
+     * 节目表id
+     */
+    private Long programId;
+    
+    /**
+     * 最低价格
+     */
+    private BigDecimal minPrice;
+    
+    /**
+     * 最高价格
+     */
+    private BigDecimal maxPrice;
+}
