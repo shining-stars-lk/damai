f31d8bdf076ecd0d20369f02a5f484cec4efaaef
阿星不是程序员 <1031900093@qq.com>
2024-01-18 17:35:12 +0800

    添加支付模块

---
+++ server/pay-service/src/main/java/com/example/PayApplication.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@MapperScan({"com.example.mapper"})
+@EnableTransactionManagement
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableHystrix
+@SpringBootApplication
+public class PayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+
+}
