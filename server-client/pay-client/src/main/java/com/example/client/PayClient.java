b6b37b50d00ef222f91e5b933529a0fd6a2c228c
阿星不是程序员 <1031900093@qq.com>
2024-01-25 18:04:41 +0800

    添加支付账单功能

--- server-client/pay-client/src/main/java/com/example/client/PayClient.java
+++ server-client/pay-client/src/main/java/com/example/client/PayClient.java
@@ -2,9 +2,13 @@ package com.example.client;
 
 import com.example.common.ApiResponse;
 import com.example.dto.PayDto;
+import com.example.dto.TradeCheckDto;
+import com.example.vo.TradeCheckVo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Map;
 
 /**
  * @program: cook-frame
@@ -18,4 +22,10 @@ public interface PayClient {
     
     @PostMapping(value = "/pay/common/pay")
     ApiResponse<String> commonPay(PayDto payDto);
+    
+    @PostMapping(value = "/pay/alipay/notify")
+    ApiResponse<String> alipayNotify(Map<String, String> params);
+    
+    @PostMapping(value = "/pay/trade/check")
+    ApiResponse<TradeCheckVo> tradeCheck(TradeCheckDto tradeCheckDto);
 }
