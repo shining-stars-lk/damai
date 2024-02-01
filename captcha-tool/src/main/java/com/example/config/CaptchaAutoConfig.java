23ddcde5dc6f33277ef4596a5653308d99bbe97c
阿星不是程序员 <1031900093@qq.com>
2024-02-01 18:46:16 +0800

    添加验证码功能

--- captcha-tool/src/main/java/com/example/config/CaptchaAutoConfig.java
+++ captcha-tool/src/main/java/com/example/config/CaptchaAutoConfig.java
@@ -1,8 +1,14 @@
 package com.example.config;
 
+import com.anji.captcha.properties.AjCaptchaProperties;
+import com.anji.captcha.service.CaptchaCacheService;
 import com.anji.captcha.service.CaptchaService;
+import com.anji.captcha.service.impl.CaptchaServiceFactory;
+import com.example.service.CaptchaCacheServiceRedisImpl;
 import com.example.service.CaptchaHandle;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
  * @program: cook-frame
@@ -15,5 +21,16 @@ public class CaptchaAutoConfig {
     @Bean
     public CaptchaHandle captchaHandle(CaptchaService captchaService){
         return new CaptchaHandle(captchaService);
+    }
+    
+    @Bean(name = "AjCaptchaCacheService")
+    @Primary
+    public CaptchaCacheService captchaCacheService(AjCaptchaProperties config, StringRedisTemplate redisTemplate){
+        //缓存类型redis/local/....
+        CaptchaCacheService ret = CaptchaServiceFactory.getCache(config.getCacheType().name());
+        if(ret instanceof CaptchaCacheServiceRedisImpl){
+            ((CaptchaCacheServiceRedisImpl)ret).setStringRedisTemplate(redisTemplate);
+        }
+        return ret;
     }
 }
