5e3975c00f6d513d8539a1452d3417b2f071cf62
shining-stars-lk <1091900093@qq.com>
2024-02-21 22:03:38 +0800

    代码规范

--- damai-id-generator-framework/src/main/java/com/baidu/fsg/uid/config/IdGeneratorRedisConfig.java
+++ damai-id-generator-framework/src/main/java/com/baidu/fsg/uid/config/IdGeneratorRedisConfig.java
@@ -13,7 +13,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  * @program: cook-frame
  * @description: 对百度开源id生成器进行redis适配
  * @see <a href="https://github.com/baidu/uid-generator/">百度开源id生成器</a>
- * @author: 星哥
+ * @author: 阿宽不是程序员
  * @create: 2023-05-23
  **/
 @Configuration(proxyBeanMethods = false)
