7447ae88470186fdd432f76ba7bf2859310fbc00
shining-stars-lk <1091900093@qq.com>
2024-02-24 23:34:16 +0800

    用户注册逻辑优化

--- damai-server/damai-user-service/src/main/java/com/damai/service/composite/register/impl/UserRegisterCountCheckHandler.java
+++ damai-server/damai-user-service/src/main/java/com/damai/service/composite/register/impl/UserRegisterCountCheckHandler.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 /**
  * @program: 极度真实还原大麦网高并发实战项目。 添加 阿宽不是程序员 微信，添加时备注 damai 来获取项目的完整资料 
- * @description: 用户注册tps检查
+ * @description: 用户注册请求数检查
  * @author: 阿宽不是程序员
  **/
 @Component
@@ -19,9 +19,6 @@ public class UserRegisterCountCheckHandler extends AbstractUserRegisterCheckHand
     @Autowired
     private RequestCounter requestCounter;
     
-    /**
-     * 验证每秒用户注册请求是否超过最大限制
-     * */
     @Override
     protected void execute(final UserRegisterDto param) {
         boolean result = requestCounter.onRequest();
