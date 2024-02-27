c4f76674e41155ca401673ff6fcfebd50ecc0e0e
shining-stars-lk <1031900093@qq.com>
2024-02-27 19:03:05 +0800

    用户注册逻辑优化

--- damai-redisson-framework/damai-redisson-service-framework/damai-bloom-filter-framework/src/main/java/com/damai/config/BloomFilterProperties.java
+++ damai-redisson-framework/damai-redisson-service-framework/damai-bloom-filter-framework/src/main/java/com/damai/config/BloomFilterProperties.java
@@ -5,16 +5,16 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
  * @program: 极度真实还原大麦网高并发实战项目。 添加 阿宽不是程序员 微信，添加时备注 damai 来获取项目的完整资料 
- * @description: 分布式锁 配置属性
+ * @description: 布隆过滤器 配置属性
  * @author: 阿宽不是程序员
  **/
 @Data
-@ConfigurationProperties(prefix = RedissonProperties.PREFIX)
-public class RedissonProperties {
+@ConfigurationProperties(prefix = BloomFilterProperties.PREFIX)
+public class BloomFilterProperties {
 
-    public static final String PREFIX = "redisson";
+    public static final String PREFIX = "bloom-filter";
     
-    private String rbLoomFilterName = "user_register_rb_loom_filter";
+    private String name;
     
     private Long expectedInsertions = 20000L;
     
--- damai-redisson-framework/damai-redisson-service-framework/damai-bloom-filter-framework/src/main/java/com/damai/handler/BloomFilterHandler.java
+++ damai-redisson-framework/damai-redisson-service-framework/damai-bloom-filter-framework/src/main/java/com/damai/handler/BloomFilterHandler.java
@@ -1,7 +1,7 @@
-package com.damai.util;
+package com.damai.handler;
 
 
-import com.damai.config.RedissonProperties;
+import com.damai.config.BloomFilterProperties;
 import org.redisson.api.RBloomFilter;
 import org.redisson.api.RedissonClient;
 
@@ -9,16 +9,16 @@ import org.redisson.api.RedissonClient;
 
 /**
  * @program: 极度真实还原大麦网高并发实战项目。 添加 阿宽不是程序员 微信，添加时备注 damai 来获取项目的完整资料 
- * @description: 布隆过滤器 方法抽象
+ * @description: 布隆过滤器
  * @author: 阿宽不是程序员
  **/
 public class BloomFilterHandler {
     
     private final RBloomFilter<String> cachePenetrationBloomFilter;
     
-    public BloomFilterHandler(RedissonClient redissonClient, RedissonProperties redissonProperties){
-        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter(redissonProperties.getRbLoomFilterName());
-        cachePenetrationBloomFilter.tryInit(redissonProperties.getExpectedInsertions(), redissonProperties.getFalseProbability());
+    public BloomFilterHandler(RedissonClient redissonClient, BloomFilterProperties bloomFilterProperties){
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter(bloomFilterProperties.getName());
+        cachePenetrationBloomFilter.tryInit(bloomFilterProperties.getExpectedInsertions(), bloomFilterProperties.getFalseProbability());
         this.cachePenetrationBloomFilter = cachePenetrationBloomFilter;
     }
     
