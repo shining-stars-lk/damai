168aaa74e8b13535090ab3f6177f3854f7b9a2a0
shining-stars-lk <1091900093@qq.com>
2024-02-29 23:05:19 +0800

    代码bug优化

--- damai-spring-cloud-framework/damai-service-common/src/main/java/com/damai/shardingsphere/DatabaseOrderComplexGeneArithmetic.java
+++ damai-spring-cloud-framework/damai-service-common/src/main/java/com/damai/shardingsphere/DatabaseOrderComplexGeneArithmetic.java
@@ -18,24 +18,12 @@ import java.util.Properties;
  * @author: 阿宽不是程序员
  **/
 public class DatabaseOrderComplexGeneArithmetic implements ComplexKeysShardingAlgorithm<Long> {
-    /**
-     * 属性分库名
-     * */
     private static final String SHARDING_COUNT_KEY_NAME = "sharding-count";
     
-    /**
-     * 属性分表名
-     * */
     private static final String TABLE_SHARDING_COUNT_KEY_NAME = "table-sharding-count";
     
-    /**
-     * 分库数量
-     * */
     private int shardingCount;
     
-    /**
-     * 分表数量
-     * */
     private int tableShardingCount;
     
     @Override
@@ -45,43 +33,35 @@ public class DatabaseOrderComplexGeneArithmetic implements ComplexKeysShardingAl
     }
     @Override
     public Collection<String> doSharding(Collection<String> allActualSplitDatabaseNames, ComplexKeysShardingValue<Long> complexKeysShardingValue) {
-        //返回的真实库名集合
         List<String> actualDatabaseNames = new ArrayList<>(allActualSplitDatabaseNames.size());
-        //查询中的列名和值
         Map<String, Collection<Long>> columnNameAndShardingValuesMap = complexKeysShardingValue.getColumnNameAndShardingValuesMap();
-        //如果没有条件查询，那么就查所有的分表
         if (CollectionUtil.isEmpty(columnNameAndShardingValuesMap)) {
             return actualDatabaseNames;
         }
         
         Collection<Long> orderNumberValues = columnNameAndShardingValuesMap.get("order_number");
-        
         Collection<Long> userIdValues = columnNameAndShardingValuesMap.get("user_id");
         
         Long value = null;
-        //如果是order_number查询
         if (CollectionUtil.isNotEmpty(orderNumberValues)) {
             value = orderNumberValues.stream().findFirst().orElseThrow(() -> new DaMaiFrameException(BaseCode.ORDER_NUMBER_NOT_EXIST));
-            //如果是user_id查询
         } else if (CollectionUtil.isNotEmpty(userIdValues)) {
             value = userIdValues.stream().findFirst().orElseThrow(() -> new DaMaiFrameException(BaseCode.USER_ID_NOT_EXIST));
         }
-        //如果order_number或者user_id的值存在
         if (Objects.nonNull(value)) {
-            //这里和分表数量进行取模，是因为订单编号中在插入user_id的基因时，是和表数量进行取模的
             long tableIndex = value % tableShardingCount;
-            //获得值后再获得实际的分库的索引
             long databaseIndex = calculateDatabaseIndex(shardingCount,tableIndex,tableShardingCount);
             String databaseIndexStr = String.valueOf(databaseIndex);
             for (String actualSplitDatabaseName : allActualSplitDatabaseNames) {
-                //将所有的分库名和得到的分库索引进行匹配
                 if (actualSplitDatabaseName.contains(databaseIndexStr)) {
                     actualDatabaseNames.add(actualSplitDatabaseName);
                     break;
                 }
             }
+            return actualDatabaseNames;
+        }else {
+            return allActualSplitDatabaseNames;
         }
-        return actualDatabaseNames;
     }
     
     /**
@@ -93,18 +73,13 @@ public class DatabaseOrderComplexGeneArithmetic implements ComplexKeysShardingAl
      * @return 分配到的数据库编号
      */
     public long calculateDatabaseIndex(long databaseCount, long tableIndex, long tableCount) {
-        // 每个数据库平均分配到的表数量（向下取整）
         long tablesPerDatabase = tableCount / databaseCount;
-        // 不能均匀分配的表的数量
         long remainder = tableCount % databaseCount;
         
-        // 计算当前tableIndex应分配到的数据库编号
         long databaseIndex;
         if (tableIndex < (tablesPerDatabase + 1) * remainder) {
-            // 如果tableIndex在能均匀分配的表的范围内
             databaseIndex = tableIndex / (tablesPerDatabase + 1);
         } else {
-            // 如果tableIndex在不能均匀分配的表的范围内
             databaseIndex = remainder + (tableIndex - (tablesPerDatabase + 1) * remainder) / tablesPerDatabase;
         }
         
