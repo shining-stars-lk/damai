cb9cd3fe8113e7cd6fc6ae6ae1851a96d2011537
阿星不是程序员 <1091900093@qq.com>
2024-03-02 20:21:32 +0800

    elasticsearch组件优化

--- damai-elasticsearch-framework/src/main/java/com/damai/conf/BusinessEsProperties.java
+++ damai-elasticsearch-framework/src/main/java/com/damai/conf/BusinessEsProperties.java
@@ -31,6 +31,4 @@ public class BusinessEsProperties {
     private Integer connectionRequestTimeOut = 40000;
     
     private Integer maxConnectNum = 400;
-    
-    private Integer maxConnectPerRoute = 400; 
 }
