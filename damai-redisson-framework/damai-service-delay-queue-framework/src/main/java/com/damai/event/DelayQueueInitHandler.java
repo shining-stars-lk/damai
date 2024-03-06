3d682c3748b8b9fdaf9ff29f61a5d91bad00e750
shining-stars-lk <1031900093@qq.com>
2024-03-06 15:44:18 +0800

    延迟队列优化

--- damai-redisson-framework/damai-service-delay-queue-framework/src/main/java/com/damai/event/DelayQueueInitHandler.java
+++ damai-redisson-framework/damai-service-delay-queue-framework/src/main/java/com/damai/event/DelayQueueInitHandler.java
@@ -23,26 +23,21 @@ public class DelayQueueInitHandler implements ApplicationListener<ApplicationSta
     
     @Override
     public void onApplicationEvent(ApplicationStartedEvent event) {
-        //获取ConsumerTask集合
+
         Map<String, ConsumerTask> consumerTaskMap = event.getApplicationContext().getBeansOfType(ConsumerTask.class);
         if (CollectionUtil.isEmpty(consumerTaskMap)) {
             return;
         }
         for (ConsumerTask consumerTask : consumerTaskMap.values()) {
-            //构建消息主题数据
             DelayQueuePart delayQueuePart = new DelayQueuePart(delayQueueBasePart,consumerTask);
-            //获取分区数
             Integer isolationRegionCount = delayQueuePart.getDelayQueueBasePart().getDelayQueueProperties()
                     .getIsolationRegionCount();
-            //根据分区数来创建消息消费队列
+            
             for(int i = 0; i < isolationRegionCount; i++) {
                 DelayConsumerQueue delayConsumerQueue = new DelayConsumerQueue(delayQueuePart, 
                         delayQueuePart.getConsumerTask().topic() + "-" + i);
-                //创建队列后监听消息
                 delayConsumerQueue.listenStart();
             }
         }
     }
-    
-    
 }
