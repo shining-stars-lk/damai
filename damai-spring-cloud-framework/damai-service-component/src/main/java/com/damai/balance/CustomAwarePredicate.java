d882b3eb91c7a144efdac8fb4b858613826cd812
阿星不是程序员 <1031900093@qq.com>
2024-03-08 14:56:49 +0800

    灰度组件优化

--- damai-spring-cloud-framework/damai-service-component/src/main/java/com/damai/balance/CustomAwarePredicate.java
+++ damai-spring-cloud-framework/damai-service-component/src/main/java/com/damai/balance/CustomAwarePredicate.java
@@ -11,13 +11,10 @@ import com.netflix.loadbalancer.ILoadBalancer;
 import com.netflix.loadbalancer.PredicateKey;
 import com.netflix.loadbalancer.Server;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -62,7 +59,7 @@ public class CustomAwarePredicate extends AbstractServerPredicate{
 	 *
 	 * 如果请求服务中请求头没有gray参数，或者该参数中的值不是true或false字符串(不区分大小写)则认为gray=false
 	 *
-	 * 如果被调用服务的 spring.cloud.nacos.discovery.metadata.gray 配置项没有配置，或者为空，或者该配置项中的值不是true或false字符串(不区分大小写)则认为mark=false
+	 * 如果被调用服务的 spring.cloud.nacos.discovery.metadata.gray 配置项没有配置，或者为空，或者该配置项中的值不是true或false字符串(不区分大小写)则认为gray=false
 	 * 判断逻辑:
 	 *   如果所有被调用服务端的配置项 --spring.cloud.nacos.discovery.metadata.gray=true，并且请求中的Header参数 gray=true，则在该请求的n次调用中apply()函数都返回true，走负载均衡
 	 *   否则被调用服务端的配置项 --spring.cloud.nacos.discovery.metadata.gray 必须与请求中的Header参数 gray 值相等 apply()函数才会返回true
@@ -75,35 +72,23 @@ public class CustomAwarePredicate extends AbstractServerPredicate{
     public boolean apply(PredicateKey input) {
 		boolean result;
 		try {
-			RequestAttributes ra = RequestContextHolder.getRequestAttributes();
-			String grayFromRequest = null;
-			if (ra != null) {
-				ServletRequestAttributes sra = (ServletRequestAttributes) ra;
-				HttpServletRequest request = sra.getRequest();
-				grayFromRequest = request.getHeader(GRAY_PARAMETER);
-			}
-			if (StringUtils.isEmpty(grayFromRequest)) {
-				grayFromRequest = BaseParameterHolder.getParameter(GRAY_PARAMETER);
-			}
-			if (StringUtils.isEmpty(grayFromRequest)) {
-				grayFromRequest = gray;
-			}
+			RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+			String grayFromRequest = Optional.ofNullable(requestAttributes)
+					.map(ra -> (ServletRequestAttributes) ra)
+					.map(sra -> sra.getRequest().getHeader(GRAY_PARAMETER))
+					.filter(StringUtil::isNotEmpty)
+					.orElseGet(() -> BaseParameterHolder.getParameter(GRAY_PARAMETER));
+			grayFromRequest = Optional.ofNullable(grayFromRequest).filter(StringUtil::isNotEmpty).orElse(gray);
 			NacosServer nacosServer = (NacosServer) input.getServer();
-			Map<String, String> metadata = nacosServer.getInstance().getMetadata();
-			String grayFromMetaData;
-			if (CollectionUtil.isEmpty(metadata)) {
-				grayFromMetaData = GRAY_FLAG_FALSE;
-			}else {
-				grayFromMetaData = metadata.get(GRAY_PARAMETER);
-			}
+			String grayFromMetaData = Optional.ofNullable(nacosServer.getInstance().getMetadata())
+					.filter(CollectionUtil::isNotEmpty)
+					.map(metadata -> metadata.get(GRAY_PARAMETER))
+					.filter(StringUtil::isNotEmpty)
+					.orElse(GRAY_FLAG_FALSE);
 			//判断如果被调用端没有灰度配置则默认配置为生产环境
-			if(StringUtil.isEmpty(grayFromMetaData) || Objects.isNull(map.get(grayFromMetaData.toLowerCase()))) {
-				grayFromMetaData = GRAY_FLAG_FALSE;
-			}
+			grayFromMetaData = Optional.ofNullable(map.get(grayFromMetaData.toLowerCase())).orElse(GRAY_FLAG_FALSE);
 			//判断如果请求端没有灰度标识则默认配置为生产环境
-			if(StringUtil.isEmpty(grayFromRequest) || Objects.isNull(map.get(grayFromRequest.toLowerCase()))) {
-				grayFromRequest = GRAY_FLAG_FALSE;
-			}
+			grayFromRequest = Optional.ofNullable(map.get(grayFromRequest.toLowerCase())).orElse(GRAY_FLAG_FALSE);
 			result = grayFromMetaData.equalsIgnoreCase(grayFromRequest);
 			
 			/*假如最后得到的结果为false，再做一次匹配
@@ -112,16 +97,13 @@ public class CustomAwarePredicate extends AbstractServerPredicate{
 			 *
 			 * 反之如果所有服务端的配置为spring.cloud.nacos.discovery.metadata.gray=true,而调用请求端的请求头中的 gray 为false，则结果返回false,不允许做负载均衡
 			 */
-			
-			if (result == false && grayFromRequest.equalsIgnoreCase(GRAY_FLAG_TRUE)) {
+			if (!result && grayFromRequest.equalsIgnoreCase(GRAY_FLAG_TRUE)) {
 				List<Server> servers = Optional.ofNullable(customEnabledRule)
 						.map(CustomEnabledRule::getLoadBalancer)
 						.map(ILoadBalancer::getReachableServers)
-						.orElseThrow(() -> new DaMaiFrameException(BaseCode.LOAD_BALANCER_NOT_EXIST));
-				if (CollectionUtil.isEmpty(servers)) {
-					throw new DaMaiFrameException(BaseCode.SERVER_LIST_NOT_EXIST);
-				}
-				List<String> judgeList = new ArrayList<String>();
+						.filter(CollectionUtil::isNotEmpty)
+						.orElseThrow(() -> new DaMaiFrameException(BaseCode.SERVER_LIST_NOT_EXIST));
+				Map<String,String> map = new HashMap<>(servers.size());
 				for (Server server : servers) {
 					NacosServer nacosServerInstance = (NacosServer) server;
 					String balanceGray = nacosServerInstance.getInstance().getMetadata().get(GRAY_PARAMETER);
@@ -129,9 +111,9 @@ public class CustomAwarePredicate extends AbstractServerPredicate{
 					if (StringUtil.isEmpty(balanceGray) || Objects.isNull(map.get(balanceGray.toLowerCase()))) {
 						balanceGray = GRAY_FLAG_FALSE;
 					}
-					judgeList.add(balanceGray);
+					map.put(balanceGray,balanceGray);
 				}
-				if(!judgeList.contains(GRAY_FLAG_TRUE)) {
+				if(Objects.isNull(map.get(GRAY_FLAG_TRUE))) {
 					result = true;
 				}
 			}
