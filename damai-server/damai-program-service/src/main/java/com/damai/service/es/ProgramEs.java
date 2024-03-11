c9157c2bd0888714379afb9057f9e69d3312872e
阿星不是程序员 <1031900093@qq.com>
2024-03-11 15:12:08 +0800

    节目服务业务优化

--- damai-server/damai-program-service/src/main/java/com/damai/service/es/ProgramEs.java
+++ damai-server/damai-program-service/src/main/java/com/damai/service/es/ProgramEs.java
@@ -1,37 +1,70 @@
-package com.damai.service.pagestrategy.impl;
+package com.damai.service.es;
 
 import cn.hutool.core.collection.CollectionUtil;
 import com.damai.core.SpringUtil;
 import com.damai.dto.EsDataQueryDto;
+import com.damai.dto.ProgramListDto;
 import com.damai.dto.ProgramPageListDto;
+import com.damai.dto.ProgramSearchDto;
 import com.damai.enums.TimeType;
 import com.damai.page.PageUtil;
 import com.damai.page.PageVo;
 import com.damai.service.init.ProgramDocumentParamName;
-import com.damai.service.pagestrategy.ProgramConstant;
-import com.damai.service.pagestrategy.SelectPageHandle;
 import com.damai.util.BusinessEsHandle;
 import com.damai.util.DateUtils;
 import com.damai.vo.ProgramListVo;
 import com.github.pagehelper.PageInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+
 /**
  * @program: 极度真实还原大麦网高并发实战项目。 添加 阿宽不是程序员 微信，添加时备注 damai 来获取项目的完整资料 
- * @description: es分页查询
+ * @description: es查询
  * @author: 阿宽不是程序员
  **/
 @Slf4j
-public class SelectPageEsHandle implements SelectPageHandle {
+@Component
+public class ProgramEs {
     
     @Autowired
     private BusinessEsHandle businessEsHandle;
-    @Override
+    
+    public Map<String, List<ProgramListVo>> selectHomeList(ProgramListDto programPageListDto) {
+        Map<String,List<ProgramListVo>> programListVoMap = new HashMap<>(256);
+        
+        try {
+            for (Long parentProgramCategoryId : programPageListDto.getParentProgramCategoryIds()) {
+                List<EsDataQueryDto> programEsQueryDto = new ArrayList<>();
+                EsDataQueryDto areaIdQueryDto = new EsDataQueryDto();
+                areaIdQueryDto.setParamName(ProgramDocumentParamName.AREA_ID);
+                areaIdQueryDto.setParamValue(programPageListDto.getAreaId());
+                programEsQueryDto.add(areaIdQueryDto);
+                EsDataQueryDto parentProgramCategoryIdQueryDto = new EsDataQueryDto();
+                parentProgramCategoryIdQueryDto.setParamName(ProgramDocumentParamName.PARENT_PROGRAM_CATEGORY_ID);
+                parentProgramCategoryIdQueryDto.setParamValue(parentProgramCategoryId);
+                programEsQueryDto.add(parentProgramCategoryIdQueryDto);
+                PageInfo<ProgramListVo> pageInfo = businessEsHandle.queryPage(
+                        SpringUtil.getPrefixDistinctionName() + "-" + ProgramDocumentParamName.INDEX_NAME,
+                        ProgramDocumentParamName.INDEX_TYPE, programEsQueryDto, 1, 7, ProgramListVo.class);
+                if (pageInfo.getList().size() > 0) {
+                    String areaName = pageInfo.getList().get(0).getAreaName();
+                    programListVoMap.put(areaName,pageInfo.getList());
+                }
+            }
+        }catch (Exception e) {
+            log.error("businessEsHandle.queryPage error",e);
+        }
+        return programListVoMap;
+    }
+    
     public PageVo<ProgramListVo> selectPage(ProgramPageListDto programPageListDto) {
         PageVo<ProgramListVo> pageVo = new PageVo<>();
         try {
@@ -85,8 +118,23 @@ public class SelectPageEsHandle implements SelectPageHandle {
         return pageVo;
     }
     
-    @Override
-    public String getType() {
-        return ProgramConstant.ES_TYPE_NAME;
+    public PageVo<ProgramListVo> search(ProgramSearchDto programSearchDto) {
+        PageVo<ProgramListVo> pageVo = new PageVo<>();
+        try {
+            List<EsDataQueryDto> esDataQueryDtoList = new ArrayList<>();
+            EsDataQueryDto titleQueryDto = new EsDataQueryDto();
+            titleQueryDto.setParamName(ProgramDocumentParamName.TITLE);
+            titleQueryDto.setParamValue(programSearchDto.getTitle());
+            titleQueryDto.setAnalyse(true);
+            esDataQueryDtoList.add(titleQueryDto);
+            PageInfo<ProgramListVo> programListVoPageInfo =
+                    businessEsHandle.queryPage(SpringUtil.getPrefixDistinctionName() + "-" + ProgramDocumentParamName.INDEX_NAME,
+                            ProgramDocumentParamName.INDEX_TYPE, esDataQueryDtoList, programSearchDto.getPageNumber(),
+                            programSearchDto.getPageSize(), ProgramListVo.class);
+            pageVo = PageUtil.convertPage(programListVoPageInfo,programListVo -> programListVo);
+        }catch (Exception e) {
+            log.error("search error",e);
+        }
+        return pageVo;
     }
 }
