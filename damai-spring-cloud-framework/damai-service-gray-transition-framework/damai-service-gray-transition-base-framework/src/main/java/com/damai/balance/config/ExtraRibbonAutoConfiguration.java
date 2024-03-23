c0e437a3a5614ab875cb6195f4659a06ea5e0ae9
阿星不是程序员 <1091900093@qq.com>
2024-03-23 13:04:27 +0800

    灰度调用功能优化

---
+++ damai-spring-cloud-framework/damai-service-gray-transition-framework/damai-service-gray-transition-base-framework/src/main/java/com/damai/balance/config/ExtraRibbonAutoConfiguration.java
@@ -0,0 +1,13 @@
+package com.damai.balance.config;
+
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
+
+/**
+ * @program: 极度真实还原大麦网高并发实战项目。 添加 阿宽不是程序员 微信，添加时备注 damai 来获取项目的完整资料 
+ * @description: 灰度版本选择相关配置
+ * @author: 阿宽不是程序员
+ **/
+
+@RibbonClients(defaultConfiguration = { WorkLoadBalanceConfiguration.class })
+public class ExtraRibbonAutoConfiguration {
+}
---
+++ damai-spring-cloud-framework/damai-service-gray-transition-framework/damai-service-gray-transition-gateway-framework/src/main/java/com/damai/context/impl/GatewayContextHolder.java
@@ -0,0 +1,31 @@
+package com.damai.context.impl;
+
+
+import org.springframework.web.server.ServerWebExchange;
+
+/**
+ * @program: 极度真实还原大麦网高并发实战项目。 添加 阿宽不是程序员 微信，添加时备注 damai 来获取项目的完整资料 
+ * @description: Gateway的ServerWebExchange数据存放
+ * @author: 阿宽不是程序员
+ **/
+public class GatewayContextHolder {
+    private static final ThreadLocal<GatewayContextHolder> THREAD_LOCAL = ThreadLocal.withInitial(GatewayContextHolder::new);
+
+    private ServerWebExchange exchange;
+
+    public static GatewayContextHolder getCurrentGatewayContext() {
+        return THREAD_LOCAL.get();
+    }
+
+    public static void removeCurrentGatewayContext() {
+        THREAD_LOCAL.remove();
+    }
+
+    public ServerWebExchange getExchange() {
+        return exchange;
+    }
+
+    public void setExchange(ServerWebExchange exchange) {
+        this.exchange = exchange;
+    }
+}
