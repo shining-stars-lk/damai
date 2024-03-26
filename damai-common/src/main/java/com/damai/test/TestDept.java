27b14f65ecf544d931bf2d3b055245112278c5cf
shining-stars-lk <1031900093@qq.com>
2024-03-26 20:09:11 +0800

    项目优化案例添加

---
+++ damai-common/src/main/java/com/damai/test/TestDept.java
@@ -0,0 +1,17 @@
+package com.damai.test;
+
+import lombok.Data;
+
+/**
+ * @program: damai
+ * @description:
+ * @author: k
+ * @create: 2024-03-26
+ **/
+@Data
+public class TestDept {
+    
+    private long id;
+    
+    private String name;
+}
