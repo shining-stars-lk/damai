5629edcdb36da6e2012b23d46a18329a85e6893d
阿星不是程序员 <1031900093@qq.com>
2024-04-10 11:00:57 +0800

    修复bug

--- damai-spring-cloud-framework/damai-service-component/src/main/java/com/damai/filter/FilterAutoConfiguration.java
+++ damai-spring-cloud-framework/damai-service-component/src/main/java/com/damai/filter/FilterAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.damai.filter;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 
 /**
  * @program: 极度真实还原大麦网高并发实战项目。 添加 阿宽不是程序员 微信，添加时备注 大麦 来获取项目的完整资料 
@@ -11,6 +12,13 @@ import org.springframework.context.annotation.Bean;
 public class FilterAutoConfiguration {
     
     @Bean
+    @Order(-10)
+    public RequestWrapperFilter requestWrapperFilter(){
+        return new RequestWrapperFilter();
+    }
+    
+    @Bean
+    @Order(1)
     public BaseParameterFilter baseParameterFilter(){
         return new BaseParameterFilter();
     }
---
+++ damai-spring-cloud-framework/damai-service-component/src/main/java/com/damai/request/CustomizeRequestWrapper.java
@@ -0,0 +1,85 @@
+package com.damai.request;
+
+import com.damai.util.StringUtil;
+
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @program: 极度真实还原大麦网高并发实战项目。 添加 阿宽不是程序员 微信，添加时备注 大麦 来获取项目的完整资料 
+ * @description: request包装
+ * @author: 阿宽不是程序员
+ **/
+public class CustomizeRequestWrapper extends HttpServletRequestWrapper {
+    
+    private final String requestBody;
+    
+    private String rewriteRequestBody;
+    
+    private boolean rewriteFlag = false;
+    
+    
+    public CustomizeRequestWrapper(HttpServletRequest request) throws IOException {
+        super(request);
+        requestBody = StringUtil.inputStreamConvertString(request.getInputStream());
+    }
+    
+    public CustomizeRequestWrapper(HttpServletRequest request, String sourceRequstBody, String rewriteRequestBody) throws IOException {
+        super(request);
+        this.rewriteFlag = true;
+        this.requestBody = sourceRequstBody;
+        this.rewriteRequestBody = rewriteRequestBody;
+        
+    }
+    
+    
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        String data = "";
+        if(rewriteFlag) {
+            data = rewriteRequestBody;
+        }else {
+            data = requestBody;
+        }
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(data.getBytes("UTF-8"));
+        return new ServletInputStream() {
+            @Override
+            public int read() throws IOException {
+                return byteArrayInputStream.read();
+            }
+            
+            @Override
+            public boolean isFinished() {
+                return false;
+            }
+            
+            @Override
+            public boolean isReady() {
+                return false;
+            }
+            
+            @Override
+            public void setReadListener(ReadListener listener) {
+                
+            }
+        };
+    }
+    
+    @Override
+    public BufferedReader getReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(this.getInputStream()));
+    }
+    
+    public String getRequestBody() {
+        return this.requestBody;
+    }
+    
+    
+    
+}
