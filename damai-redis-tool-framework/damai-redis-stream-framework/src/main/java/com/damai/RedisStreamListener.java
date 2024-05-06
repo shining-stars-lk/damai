36b9e17d4e01f1a1825245987b5eadacffb0a22e
阿星不是程序员 <1091900093@qq.com>
2024-05-06 22:35:45 +0800

    添加RedisStream

--- damai-redis-tool-framework/damai-redis-stream-framework/src/main/java/com/damai/RedisStreamListener.java
+++ damai-redis-tool-framework/damai-redis-stream-framework/src/main/java/com/damai/RedisStreamListener.java
@@ -1,16 +1,11 @@
 package com.damai;
 
-import com.damai.enums.BaseCode;
-import com.damai.exception.DaMaiFrameException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationContext;
 import org.springframework.data.redis.connection.stream.ObjectRecord;
 import org.springframework.data.redis.connection.stream.RecordId;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.stream.StreamListener;
-
-import java.util.Optional;
 
 /**
  * @program: 极度真实还原大麦网高并发实战项目。 添加 阿宽不是程序员 微信，添加时备注 大麦 来获取项目的完整资料 
@@ -25,7 +20,7 @@ public class RedisStreamListener implements StreamListener<String, ObjectRecord<
     
     private final RedisStreamConfigProperties redisStreamConfigProperties;
     
-    private final ApplicationContext applicationContext;
+    private final MessageConsumer messageConsumer;
 
     @Override
     protected void finalize() throws Throwable {
@@ -41,8 +36,7 @@ public class RedisStreamListener implements StreamListener<String, ObjectRecord<
             String value = message.getValue();
             log.info("redis stream 消费到了数据 messageId : {}, streamName : {}, message : {}", messageId, message.getStream(), value);
             //处理消息
-            Optional.of(applicationContext.getBean(MessageConsumer.class))
-                    .orElseThrow(() -> new DaMaiFrameException(BaseCode.MESSAGE_CONSUMER_NOT_EXIST)).accept(message);
+            messageConsumer.accept(message);
             //手动提交ack
             this.stringRedisTemplate.opsForStream().acknowledge(redisStreamConfigProperties.getConsumerGroup(), message);
         }catch (Exception e){
