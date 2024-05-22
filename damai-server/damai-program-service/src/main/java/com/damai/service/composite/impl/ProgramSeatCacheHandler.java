d7a4d268c594f9b8e5dea4c7eb60d8bd9e789faf
shining-stars-lk <1031900093@qq.com>
2024-05-22 13:43:22 +0800

    优化用户下单验证逻辑

--- damai-server/damai-program-service/src/main/java/com/damai/service/composite/impl/ProgramSeatCacheHandler.java
+++ damai-server/damai-program-service/src/main/java/com/damai/service/composite/impl/ProgramSeatCacheHandler.java
@@ -3,34 +3,37 @@ package com.damai.service.composite.impl;
 import com.damai.core.RedisKeyManage;
 import com.damai.dto.ProgramOrderCreateDto;
 import com.damai.entity.ProgramShowTime;
-import com.damai.enums.BaseCode;
-import com.damai.exception.DaMaiFrameException;
 import com.damai.redis.RedisCache;
 import com.damai.redis.RedisKeyBuild;
+import com.damai.service.SeatService;
 import com.damai.service.composite.AbstractProgramCheckHandler;
+import com.damai.util.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @program: 极度真实还原大麦网高并发实战项目。 添加 阿星不是程序员 微信，添加时备注 大麦 来获取项目的完整资料 
- * @description: 节目演出时间检查
+ * @description: 将节目座位缓存
  * @author: 阿星不是程序员
  **/
 @Component
-public class ProgramShowTimeCheckHandler extends AbstractProgramCheckHandler {
+public class ProgramSeatCacheHandler extends AbstractProgramCheckHandler {
     
     @Autowired
     private RedisCache redisCache;
+    
+    @Autowired
+    private SeatService seatService;
     
     @Override
     protected void execute(final ProgramOrderCreateDto programOrderCreateDto) {
         ProgramShowTime programShowTime = redisCache.get(RedisKeyBuild.createRedisKey(RedisKeyManage.PROGRAM_SHOW_TIME
                 ,programOrderCreateDto.getProgramId()),ProgramShowTime.class);
-        if (Objects.isNull(programShowTime)) {
-            throw new DaMaiFrameException(BaseCode.PROGRAM_SHOW_TIME_NOT_EXIST);
-        }
+        
+        seatService.selectSeatByProgramId(programOrderCreateDto.getProgramId(),
+                DateUtils.countBetweenSecond(DateUtils.now(), programShowTime.getShowTime()), TimeUnit.SECONDS);
     }
     
     @Override
@@ -45,6 +48,6 @@ public class ProgramShowTimeCheckHandler extends AbstractProgramCheckHandler {
     
     @Override
     public Integer executeOrder() {
-        return 3;
+        return 4;
     }
 }
