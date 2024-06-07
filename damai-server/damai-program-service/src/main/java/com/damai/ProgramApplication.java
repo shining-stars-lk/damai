05ab755f07d780f559023ab6ec1c862704051970
shining-stars-lk <1031900093@qq.com>
2024-06-07 14:46:45 +0800

    定时重置节目数据

--- damai-server/damai-program-service/src/main/java/com/damai/ProgramApplication.java
+++ damai-server/damai-program-service/src/main/java/com/damai/ProgramApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 /**
  * @program: 极度真实还原大麦网高并发实战项目。 添加 阿星不是程序员 微信，添加时备注 大麦 来获取项目的完整资料 
@@ -17,6 +18,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
+@EnableScheduling
 @SpringBootApplication
 public class ProgramApplication {
 
