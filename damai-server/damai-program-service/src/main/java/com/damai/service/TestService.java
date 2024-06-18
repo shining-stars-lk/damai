28dd1f86bd1d1f794f34b5fde1c2bf05a2958dcc
阿星不是程序员 <1031900093@qq.com>
2024-06-18 18:09:00 +0800

    添加压测接口

---
+++ damai-server/damai-program-service/src/main/java/com/damai/service/TestService.java
@@ -0,0 +1,47 @@
+package com.damai.service;
+
+import com.alibaba.fastjson2.JSON;
+import com.baidu.fsg.uid.utils.PaddedAtomicLong;
+import com.damai.context.DelayQueueContext;
+import com.damai.dto.TestSendDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @program: 极度真实还原大麦网高并发实战项目。 添加 阿星不是程序员 微信，添加时备注 大麦 来获取项目的完整资料 
+ * @description: Test service
+ * @author: 阿星不是程序员
+ **/
+@Slf4j
+@Service
+public class TestService {
+    
+    @Autowired
+    private DelayQueueContext delayQueueContext;
+    
+    AtomicLong count = new PaddedAtomicLong(0);
+    
+    public boolean testSend(TestSendDto testSendDto) {
+        try {
+            testSendDto.setTime(System.currentTimeMillis());
+            testSendDto.setCount(count.incrementAndGet());
+            String message = JSON.toJSONString(testSendDto);
+            delayQueueContext.sendMessage("test-topic",
+                    message, 5000, TimeUnit.MILLISECONDS);
+            log.info("发送消息 : {}",message);
+        }catch (Exception e) {
+            log.error("test send message error message : {}",JSON.toJSONString(testSendDto),e);
+            return false;
+        }
+        return true;
+    }
+    
+    public Boolean reset(final TestSendDto testSendDto) {
+        count.set(0);
+        return true;
+    }
+}
