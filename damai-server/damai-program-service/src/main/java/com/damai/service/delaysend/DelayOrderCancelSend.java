dcd5f4484bb45667c35e380027e7aa4d0ce08ab2
lukuan <lukuan@hessianhealth.com>
2024-06-26 18:14:27 +0800

    修改延迟队列时间

--- damai-server/damai-program-service/src/main/java/com/damai/service/delaysend/DelayOrderCancelSend.java
+++ damai-server/damai-program-service/src/main/java/com/damai/service/delaysend/DelayOrderCancelSend.java
@@ -1,9 +1,14 @@
 package com.damai.service.delaysend;
 
 import com.damai.context.DelayQueueContext;
+import com.damai.core.SpringUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import static com.damai.service.constant.ProgramOrderConstant.DELAY_ORDER_CANCEL_TIME;
+import static com.damai.service.constant.ProgramOrderConstant.DELAY_ORDER_CANCEL_TIME_UNIT;
+import static com.damai.service.constant.ProgramOrderConstant.DELAY_ORDER_CANCEL_TOPIC;
 
 /**
  * @program: 极度真实还原大麦网高并发实战项目。 添加 阿星不是程序员 微信，添加时备注 大麦 来获取项目的完整资料 
@@ -18,13 +23,13 @@ public class DelayOrderCancelSend {
     private DelayQueueContext delayQueueContext;
     
     public void sendMessage(String message){
-//        try {
-//            log.info("延迟订单取消消息进行发送 消息体 : {}",message);
-//            delayQueueContext.sendMessage(SpringUtil.getPrefixDistinctionName() + "-" + DELAY_ORDER_CANCEL_TOPIC,
-//                    message, DELAY_ORDER_CANCEL_TIME, DELAY_ORDER_CANCEL_TIME_UNIT);
-//        }catch (Exception e) {
-//            log.error("send message error message : {}",message,e);
-//        }
+        try {
+            log.info("延迟订单取消消息进行发送 消息体 : {}",message);
+            delayQueueContext.sendMessage(SpringUtil.getPrefixDistinctionName() + "-" + DELAY_ORDER_CANCEL_TOPIC,
+                    message, DELAY_ORDER_CANCEL_TIME, DELAY_ORDER_CANCEL_TIME_UNIT);
+        }catch (Exception e) {
+            log.error("send message error message : {}",message,e);
+        }
         
     }
 }
