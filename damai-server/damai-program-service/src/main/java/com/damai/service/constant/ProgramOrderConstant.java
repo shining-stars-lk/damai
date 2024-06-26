f0bb680bdc5654d05f66433af508cded4af42b1a
阿星不是程序员 <1091900093@qq.com>
2024-06-26 18:14:27 +0800

    修改延迟队列时间

--- damai-server/damai-program-service/src/main/java/com/damai/service/constant/ProgramOrderConstant.java
+++ damai-server/damai-program-service/src/main/java/com/damai/service/constant/ProgramOrderConstant.java
@@ -11,9 +11,9 @@ public class ProgramOrderConstant {
     
     public static final String DELAY_ORDER_CANCEL_TOPIC ="d_delay_order_cancel_topic";
     
-    public static final Long DELAY_ORDER_CANCEL_TIME = 5L;
+    public static final Long DELAY_ORDER_CANCEL_TIME = 10L;
     
-    public static final TimeUnit DELAY_ORDER_CANCEL_TIME_UNIT = TimeUnit.SECONDS;
+    public static final TimeUnit DELAY_ORDER_CANCEL_TIME_UNIT = TimeUnit.MINUTES;
     
     public static final String DELAY_OPERATE_PROGRAM_DATA_TOPIC = "d_delay_operate_program_data_topic";
     
