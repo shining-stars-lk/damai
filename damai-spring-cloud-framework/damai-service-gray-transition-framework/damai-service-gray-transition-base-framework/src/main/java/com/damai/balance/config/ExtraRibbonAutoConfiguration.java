8ffb5d4cd7a840435093f24e8124c11f8f906c2e
阿星不是程序员 <1091900093@qq.com>
2024-07-16 15:09:12 +0800

    升级springboot版本

--- damai-spring-cloud-framework/damai-service-gray-transition-framework/damai-service-gray-transition-base-framework/src/main/java/com/damai/balance/config/ExtraRibbonAutoConfiguration.java
+++ damai-spring-cloud-framework/damai-service-gray-transition-framework/damai-service-gray-transition-base-framework/src/main/java/com/damai/balance/config/ExtraRibbonAutoConfiguration.java
@@ -1,13 +1,11 @@
 package com.damai.balance.config;
 
-import org.springframework.cloud.netflix.ribbon.RibbonClients;
-
 /**
  * @program: 极度真实还原大麦网高并发实战项目。 添加 阿星不是程序员 微信，添加时备注 大麦 来获取项目的完整资料 
  * @description: 灰度版本选择相关配置
  * @author: 阿星不是程序员
  **/
 
-@RibbonClients(defaultConfiguration = { WorkLoadBalanceConfiguration.class })
+
 public class ExtraRibbonAutoConfiguration {
 }
