1314f89588a83645b871faf4cc83741ca9a312fc
阿星不是程序员 <1091900093@qq.com>
2024-07-24 15:35:21 +0800

    修改节目详情版本

--- damai-server/damai-program-service/src/main/java/com/damai/service/ProgramService.java
+++ damai-server/damai-program-service/src/main/java/com/damai/service/ProgramService.java
@@ -362,7 +362,7 @@ public class ProgramService extends ServiceImpl<ProgramMapper, Program> {
     
     public ProgramVo detail(ProgramGetDto programGetDto) {
         compositeContainer.execute(CompositeCheckType.PROGRAM_DETAIL_CHECK.getValue(),programGetDto);
-        return getDetailV2(programGetDto);
+        return getDetail(programGetDto);
     }
     
     public ProgramVo detailV1(ProgramGetDto programGetDto) {
