457c765eb1fe341c85f2981f8f390d7bfac9b714
阿星不是程序员 <1031900093@qq.com>
2024-09-29 15:25:11 +0800

    修复服务监听跳转错误问题

--- damai-server/damai-admin-service/src/main/java/com/damai/config/AdminConfig.java
+++ damai-server/damai-admin-service/src/main/java/com/damai/config/AdminConfig.java
@@ -1,5 +1,8 @@
 package com.damai.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
@@ -17,5 +20,19 @@ public class AdminConfig {
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jacksonCustomEnhance(){
         return new JacksonCustomEnhance();
+    }
+    
+    @Bean
+    public OpenAPI customOpenApi() {
+        
+        return new OpenAPI()
+                .info(new Info()
+                        .title("前端使用")
+                        .version("1.0")
+                        .description("项目学习")
+                        .contact(new Contact()
+                                .name("阿星不是程序员")
+                        ));
+        
     }
 }
--- damai-server/damai-admin-service/src/main/java/com/damai/config/SecurityConfig.java
+++ damai-server/damai-admin-service/src/main/java/com/damai/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(authorize -> authorize
                     .requestMatchers(adminContextPath + "/assets/**").permitAll()
                     .requestMatchers(adminContextPath + "/login", "/css/**", "/js/**", "/image/*").permitAll()
+                    .requestMatchers(adminContextPath + "/v3/api-docs/**").permitAll()
                     .anyRequest().authenticated())
             .formLogin(form -> form
                     .loginPage(adminContextPath + "/login")
@@ -42,8 +43,8 @@ public class SecurityConfig {
             .logout(logout -> logout
                     .logoutUrl(adminContextPath + "/logout"))
             .httpBasic(withDefaults())
-            .csrf(csrf -> csrf.ignoringRequestMatchers("/instances", "/actuator/**",
-                    adminContextPath + "/instances/**", adminContextPath + "/actuator/**"));    
+            .csrf(csrf -> csrf.ignoringRequestMatchers("/instances", "/actuator/**","/v3/api-docs/**",
+                    adminContextPath + "/instances/**", adminContextPath + "/actuator/**", adminContextPath + "/v3/api-docs/**"));    
         return http.build();
     }
 }
