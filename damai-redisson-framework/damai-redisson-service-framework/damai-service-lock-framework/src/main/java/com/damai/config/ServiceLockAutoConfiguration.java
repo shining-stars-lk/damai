768693cde019025515bc60c48d33498fb6ee7e6b
阿星不是程序员 <1031900093@qq.com>
2024-10-18 18:31:55 +0800

    优化锁的实例管理行为

--- damai-redisson-framework/damai-redisson-service-framework/damai-service-lock-framework/src/main/java/com/damai/config/ServiceLockAutoConfiguration.java
+++ damai-redisson-framework/damai-redisson-service-framework/damai-service-lock-framework/src/main/java/com/damai/config/ServiceLockAutoConfiguration.java
@@ -23,17 +23,11 @@ public class ServiceLockAutoConfiguration {
         return new ServiceLockInfoHandle();
     }
     
-    /**
-     * 锁管理
-     * */
     @Bean
     public ManageLocker manageLocker(RedissonClient redissonClient){
         return new ManageLocker(redissonClient);
     }
     
-    /**
-     * 锁工厂
-     * */
     @Bean
     public ServiceLockFactory serviceLockFactory(ManageLocker manageLocker){
         return new ServiceLockFactory(manageLocker);
